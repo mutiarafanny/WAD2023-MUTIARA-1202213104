b2122a5a97166325e35d32fc65f6378ec0b2722f
mutiarafanny <hi.mutiarafanny@gmail.com>
2023-11-14 11:36:10 +0700

    Modul 3

---
+++ MODUL3 MUTIARA/connect.php
@@ -0,0 +1,18 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+
+<?php
+// Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+    $host = "localhost";
+    $username = "root";
+    $pass = "";
+    $db_name = "wad_modul3_mutiara";
+    $connect = mysqli_connect($host, $username, $pass, $db_name);
+// 
+  
+// Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
+    if ($connect -> connect_error){
+        die("Koneksi gagal!".$connect -> connect_error);
+    }
+// 
+?>
